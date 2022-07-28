1171a12554dbd6e61487914328abefe22a50bbb3
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-07-28 12:19:35 +0900

    エラーメッセージを変更

--- pages/task-page.tsx
+++ pages/task-page.tsx
@@ -22,7 +22,7 @@ const TaskPage: React.FC<STATICPROPS> = ({ staticTasks }) => {
     revalidateOnMount: true,
   })
 
-  if (error) return <span>Error!</span>
+  if (error) return <span>Eror!</span>
 
   return (
     <Layout title="todos">
